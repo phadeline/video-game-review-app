8bc586b90c4bfc6ea1234871c415e0938fa89268
phadeline <112015433+phadeline@users.noreply.github.com>
2023-02-20 18:05:33 -0500

    changes

--- controllers/api/review-routes.js
+++ controllers/api/review-routes.js
@@ -45,7 +45,7 @@ router.delete('/:id', async (req, res) => {
       res.status(404).json({ message: 'No review found with that id!' });
       return;
     }
-    res.status(200).json(readerData);
+    res.status(200).json(reviewData);
 
   } catch (err) {
     res.status(500).json(err);
